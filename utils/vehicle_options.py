1a39826fc21d524526b0a7edf873d98c0d5087d1
NatesHonor <nathaniel.j.marcellus@gmail.com>
2025-01-30 08:06:47 -0500

    Added new vehicle options

--- utils/vehicle_options.py
+++ utils/vehicle_options.py
@@ -1,14 +1,17 @@
 def get_vehicle_options(vehicle_type):
     vehicle_options_map = {
         "firetruck": ["Type 1 fire engine", "Type 2 fire engine", "Type 3 fire engine"],
+        "firetrucks, heavy rescue vehicles or platform truck": ["Type 1 fire engine", "Type 2 fire engine", "Platform truck", "Quint", "Heavy rescue vehicle", "Rescue Engine"],
         "platform truck": ["Platform truck", "Quint"],
         "battalion chief vehicle": ["Battalion chief unit"],
+        "mobile air": ["Mobile air"],
         "mobile air unit": ["Mobile air"],
         "mobile air vehicle": ["Mobile air"],
         "heavy rescue vehicle": ["Heavy rescue vehicle", "Rescue Engine"],
         "hazmat vehicle": ["HazMat"],
         "mobile command vehicle": ["MCV"],
         "fire investigation unit": ["Fire Investigator Unit"],
+        "arff": ["Small ARFF Crash Tender", "Medium ARFF Crash Tender", "Large ARFF Crash Tender"],
         "ambulance": ["ALS Ambulance", "BLS Ambulance"],
         "police car": ["Patrol car"],
         "police supervisors / sheriff": ["Police Supervisor / Sheriff Unit"],
@@ -16,7 +19,10 @@ def get_vehicle_options(vehicle_type):
         "fbi investigation wagon": ["FBI Investigation Wagon"],
         "riot police unit": ["Riot Police Van", "Riot Police Bus"],
         "warden truck": ["Warden's Truck"],
-        "police cars or swat suv": ["Patrol car", "SWAT SUV"]
+        "police cars or swat suv": ["Patrol car", "SWAT SUV"],
+        "wildland fire engine": ["Type 3 engine", "Type 4 engine", "Type 5 engine", "Type 6 engine", "Type 7 engine"],
+        "wildland fire vehicle": ["Type 3 engine", "Type 4 engine", "Type 5 engine", "Type 6 engine", "Type 7 engine"]
+
     }
     vehicle_type = vehicle_type.lower()
     return vehicle_options_map.get(vehicle_type, [])
